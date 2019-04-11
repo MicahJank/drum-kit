8fa3b75d25a9a80f70c281876911d0101834467d
MicahJank <MicahJank@users.noreply.github.com>
2019-04-11 11:59:37 -0500

    drum-kit
    added javascript function to change text color of buttons when clicked as well as added background images to the buttons in css

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const drumArray = document.querySelectorAll(".drum");
 
 for(let i = 0; i < drumArray.length; i++) {
-    drumArray[i].addEventListener("click", handleClick);
+    drumArray[i].addEventListener("click", function() {
+        this.style.color = "white";
+    });
 }
 
-function handleClick() {
-    alert("I got clicked!");
-}
 
+// const tom1Sound = new Audio("sounds/tom-1.mp3");
+//         tom1Sound.play();
